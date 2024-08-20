1069fcedb0ed41bc4c0525d0ec6b2bb733b4f03f
Lunar <161990111+LunarrBlue@users.noreply.github.com>
2024-08-20 16:35:34 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ python -m pip install --upgrade pip
 echo Installed pip
 
 echo Installing Packages
-pip install pyinstaller psutil discord.py pyautogui numpy opencv-python pyperclip cryptography pyttsx3 requests pywin32 pycryptodome comtypes
+pip install pyinstaller psutil discord.py pyautogui numpy opencv-python pyperclip cryptography pyttsx3 requests pywin32 pycryptodome
 echo Installed Packages
 
 echo Compiling scripts
